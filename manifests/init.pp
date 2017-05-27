f6dcce87f18bee85f0695a1d254f26edd7b0c766
Christopher Lord <christopher.lord@mnfgroup.limited>
2017-05-27 22:46:19 +1000

    Ensure RPM key is imported after yum repo is downloaded

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,8 +5,16 @@ class jenkins () {
     source => 'http://pkg.jenkins-ci.org/redhat/jenkins.repo',
   }
 
+  exec { 'install_key':
+    path        => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
+    command     => 'sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key',
+    refreshonly => true,
+  }
+
   package { 'jenkins':
     ensure => latest,
   }
-  
+
+  File['jenkins.repo'] ~> Exec['install_key'] ->
+  Package['jenkins']  
 }
